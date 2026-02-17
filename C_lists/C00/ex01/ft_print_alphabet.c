b19e3b652cd26cb908ac27d3c3b1f924cca41944
julio pestanas <juliopestana@LAPTOP-QKKJLTBK.localdomain>
2026-02-17 18:40:23 -0300

    criando as primeiras soluções

---
+++ C_lists/C00/ex01/ft_print_alphabet.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_alphabet.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 18:33:15 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/02/17 18:38:24 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_alphabet(void)
+{
+	char	count;
+
+	count = 'a';
+	while (count <= 'z')
+	{
+		write(1, &count, 1);
+		count++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_alphabet();
+	write(1, "\n", 1);
+	return (0);
+}*/
