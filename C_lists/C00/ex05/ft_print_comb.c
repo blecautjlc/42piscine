f3e63e8f628578b03b5f054c59fc6ee6796e5bb6
julio pestanas <juliopestana@LAPTOP-QKKJLTBK.localdomain>
2026-02-18 15:36:09 -0300

    ex05

---
+++ C_lists/C00/ex05/ft_print_comb.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_comb.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/17 21:50:10 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/02/17 23:04:24 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_result(char a, char b, char c)
+{
+	ft_putchar(a);
+	ft_putchar(b);
+	ft_putchar(c);
+	if (!(a == '7' && b == '8' && c == '9'))
+		write(1, ", ", 2);
+}
+
+void	ft_print_comb(void)
+{
+	char	a;
+	char	b;
+	char	c;
+
+	a = '0';
+	while (a <= '7')
+	{
+		b = a + 1;
+		while (b <= '8')
+		{
+			c = b + 1;
+			while (c <= '9')
+			{
+				ft_result(a, b, c);
+				c++;
+			}
+			b++;
+		}
+		a++;
+	}
+}
+
+/*int	main(void)
+{
+	ft_print_comb();
+	return (0);
+}*/
