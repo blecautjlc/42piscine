553adee3d462e2ffa07ecf41c11ce546476042ce
julio pestanas <juliopestana@LAPTOP-QKKJLTBK.localdomain>
2026-02-20 19:18:12 -0300

    ex00

---
+++ C_lists/C01/ex00/ft_ft.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ft.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/20 19:08:59 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/02/20 19:17:22 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ft(int *nbr)
+{
+	*nbr = 42;
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	int	x;
+
+	x = 10;
+	printf("Valor de x antes da função: %d\n", x);
+	ft_ft(&x);
+	printf("Valor da x depois de função: %d\n", x);
+	return (0);
+}*/
