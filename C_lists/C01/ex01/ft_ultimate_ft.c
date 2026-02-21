4c42b0f6c0718aa7d30a9be313d7250cd69d2762
julio pestanas <juliopestana@LAPTOP-QKKJLTBK.localdomain>
2026-02-21 12:02:03 -0300

    ex01

---
+++ C_lists/C01/ex01/ft_ultimate_ft.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ultimate_ft.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 11:42:03 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/02/21 12:00:26 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_ultimate_ft(int *********nbr)
+{
+	*********nbr = 42;
+}
+
+/*#include<stdio.h>
+
+int	main(void)
+{
+	int	x;
+	
+	int	*p1 = &x;
+	int	**p2 = &p1;
+	int	***p3 = &p2;
+	int	****p4 = &p3;
+	int	*****p5 = &p4;
+	int	******p6 = &p5;
+	int	*******p7 = &p6;
+	int	********p8 = &p7;
+	int	*********p9 = &p8;
+	
+	x = 10;
+	printf("Valor de x antes: %d\n", x);
+	ft_ultimate_ft(p9);
+	printf("Valor de x depois %d\n", x);
+	return (0);
+}*/
