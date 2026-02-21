c130f311630c2f86154c5b914b4fcc87e3645b34
julio pestanas <juliopestana@LAPTOP-QKKJLTBK.localdomain>
2026-02-21 16:59:36 -0300

    ex05

---
+++ C_lists/C01/ex05/ft_putstr.c
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/21 16:46:24 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/02/21 16:58:33 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putstr(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		write(1, &str[i], 1);
+		i++;
+	}
+}
+
+/*int	main(void)
+{
+	char str[] = "Hello World";
+	
+	ft_putstr(str);
+	write(1, "\n", 1);
+	return (0);
+}*/
