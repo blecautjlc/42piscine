bde20697a034ceab24a5459c362e1ef8257b762b
julio pestanas <juliopestana@LAPTOP-QKKJLTBK.localdomain>
2026-02-26 20:31:06 -0300

    ex08

---
+++ C_lists/C02/ex08/ft_strlowcase.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlowcase.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 20:21:50 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/02/26 20:30:36 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strlowcase(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] >= 'A' && str[i] <= 'Z')
+			str[i] += 32;
+		i++;
+	}
+	return (str);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char	str0[] = "AAa bBbB < , CCCC";
+	ft_strlowcase(str0);
+	printf("%s\n", str0);
+	return (0);
+}*/
