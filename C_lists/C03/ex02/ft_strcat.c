94e83035a704782e5fc9b6ea3d38719a91969001
julio pestana <blecautjlc@gmail.com>
2026-03-07 20:15:33 -0300

    ex02

---
+++ C_lists/C03/ex02/ft_strcat.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcat.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: juliopestana <juliopestana@student.42.f    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/03/07 19:46:38 by juliopestan       #+#    #+#             */
+/*   Updated: 2026/03/07 20:10:12 by juliopestan      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+char	*ft_strcat(char *dest, char *src)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (dest[i])
+		i++;
+	j = 0;
+	while (src[j])
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+
+/*#include <stdio.h>
+
+int	main(void)
+{
+	char	a[] = "Hello ";
+	char	b[] = "worlrd!";
+
+	printf("%s\n", ft_strcat(a, b));
+	return (0);
+}*/
